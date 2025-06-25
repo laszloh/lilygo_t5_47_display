b9d3887e7638a946b57a6248088395128a3a81dc
Laszlo Hegedüs <laszlo.hegedues@gmail.com>
2025-06-25 15:41:16 +0200

    make adc measurement more robust

--- components/lilygo_t5_47_battery/Lilygot547Battery.cpp
+++ components/lilygo_t5_47_battery/Lilygot547Battery.cpp
@@ -1,34 +1,59 @@
 #include "Lilygot547Battery.h"
 #include "esphome/core/log.h"
 
+#define TAG "Lilygot547Battery"
+
+
 namespace esphome {
 namespace lilygo_t5_47_battery {
 
-void Lilygot547Battery::setup() {}
+void Lilygot547Battery::setup() {
+  adc1_config_width(ADC_WIDTH_BIT_12);
+  adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
+  correct_adc_reference();
+}
 
 void Lilygot547Battery::update() {
   epd_poweron();
   // wait for voltage to stabilise
   delay(100);
-  Lilygot547Battery::update_battery_info();
+  this->update_battery_info();
   epd_poweroff();
 }
+
 void Lilygot547Battery::update_battery_info() {
-  Lilygot547Battery::correct_adc_reference();
-  // 14 Battery on T5-4.7 inch ESP32-S3 (aka "plus"/V2.3)
-  uint16_t v = analogRead(36);
-  double_t battery_voltage = ((double_t) v / 4095.0) * 2.0 * 3.3 * (this->vref / 1000.0);
-  if (this->voltage != nullptr) {
-    this->voltage->publish_state(battery_voltage);
+  static constexpr size_t samples = 8;
+  uint32_t aggregator = 0;
+
+  for(uint8_t i = 0; i < samples; i++) {
+    int raw = adc1_get_raw(ADC1_CHANNEL_0);   // GPIO36
+    if(raw == -1) {
+      ESP_LOGE(TAG, "ADC measurement failed!");
+      if (this->voltage != nullptr)
+        this->voltage->publish_state(NAN);
+      return;
+    }
+    aggregator += raw;
   }
+  uint32_t mv = esp_adc_cal_raw_to_voltage(aggregator / samples, &this->cal_characteristics_);
+
+  float battery_voltage = mv * 2.0f / 1000.0f;
+  if (this->voltage != nullptr)
+    this->voltage->publish_state(battery_voltage);
 }
 
 void Lilygot547Battery::correct_adc_reference() {
-  esp_adc_cal_characteristics_t adc_chars;
-  esp_adc_cal_value_t val_type =
-      esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
-  if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
-    this->vref = adc_chars.vref;
+  esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &this->cal_characteristics_);
+  switch (val_type) {
+    case ESP_ADC_CAL_VAL_EFUSE_VREF:
+      ESP_LOGV(TAG, "Using eFuse Vref for calibration");
+      break;
+    case ESP_ADC_CAL_VAL_EFUSE_TP:
+      ESP_LOGV(TAG, "Using two-point eFuse Vref for calibration");
+      break;
+    case ESP_ADC_CAL_VAL_DEFAULT_VREF:
+    default:
+      break;
   }
 }
 
--- components/lilygo_t5_47_battery/Lilygot547Battery.h
+++ components/lilygo_t5_47_battery/Lilygot547Battery.h
@@ -19,13 +19,15 @@ class Lilygot547Battery : public PollingComponent {
  public:
   sensor::Sensor *voltage{nullptr};
 
-  int vref = 1100;
   void setup() override;
   void update() override;
   void update_battery_info();
   void correct_adc_reference();
 
   void set_voltage_sensor(sensor::Sensor *voltage_sensor) { voltage = voltage_sensor; }
+
+ protected:
+  esp_adc_cal_characteristics_t cal_characteristics_;
 };
 }  // namespace lilygo_t5_47_battery
 }  // namespace esphome
